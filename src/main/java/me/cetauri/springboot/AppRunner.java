ed67bfff18213cabd2f4a09845fabff53d35b55f
Kwon, oh-sang <cetauri@gmail.com>
2019-10-16 19:59:48 +0200

    ResourceLoader Sample

--- src/main/java/me/cetauri/springboot/AppRunner.java
+++ src/main/java/me/cetauri/springboot/AppRunner.java
@@ -4,12 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Component
 public class AppRunner implements ApplicationRunner {
     @Autowired
     ApplicationContext context;
+
+    @Autowired
+    ResourceLoader resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -17,6 +25,8 @@ public class AppRunner implements ApplicationRunner {
         System.out.println("app.name:" + env.getProperty("app.name"));
         System.out.println("app.test:" + env.getProperty("app.test"));
 
-
+        URI uri = resourceLoader.getResource("classpath:/test.txt").getURI();
+        String txt = Files.readString(Path.of(uri));
+        System.out.println(txt);
     }
 }
