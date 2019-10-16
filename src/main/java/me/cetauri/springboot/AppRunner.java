fa0bf6e94a149fb8feade1b678cdea9b123666c2
Kwon, oh-sang <cetauri@gmail.com>
2019-10-16 19:02:11 +0200

    Properties sample

---
+++ src/main/java/me/cetauri/springboot/AppRunner.java
@@ -0,0 +1,19 @@
+package me.cetauri.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+    @Autowired
+    ApplicationContext context;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        var env = context.getEnvironment();
+        System.out.println("app.name:" + env.getProperty("app.name"));
+    }
+}
